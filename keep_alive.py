fc81355f37f15db748c3d02b53d29433d2ca1b96
spinfal <spinfal@atheist.com>
2020-11-10 19:42:27 +0000

    updated/fixed random method

--- keep_alive.py
+++ keep_alive.py
@@ -1,12 +1,19 @@
 from flask import Flask
 #from threading import Thread
 from termcolor import colored as cl
-from random_word import RandomWords
+import random
+import string
 import requests, os
 import time as t
-r = RandomWords()
 
-randomSet = r.get_random_word()
+def get_random_string(length):
+    random_list = []
+    for i in range(length):
+      random_list.append(random.choice(string.ascii_uppercase + string.digits))
+    return ''.join(random_list)
+length = [5, 6, 7]
+randomSet = get_random_string(random.choice(length)).lower()
+
 #set flask app name
 name = input('flask app name (leave blank for random): ')
 if name == "":
@@ -37,9 +44,13 @@ app = Flask(name)
 def main():
   return content
 
+@app.route('/')
+def index():
+  return "<center><p>and i oop-</p><br><br><a href='/" + route + "'>your route</a></center>"
+
 def run():
   app.run(host="0.0.0.0", port=0)
 print(cl('starting your hydroflask...', 'cyan'))
-print(cl('started at: 0.0.0.0/' + route, 'green'))
+print(cl('started at: https://hydroflask.spinfal.repl.co/' + route, 'green'))
 t.sleep(2)
 run()
